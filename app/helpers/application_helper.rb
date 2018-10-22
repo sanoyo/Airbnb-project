6e4715a91f43d9f1731edca5ab19ab6d5009dd06
sanoyo <sa00380304yo@gmail.com>
2018-10-23 05:52:53 +0900

    Fix Authenticate Design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def avatar_url(user)
+		gravatar_id = Digest::MDS::hexdigest(user.email).downcase
+		"https://www.gravatarcom/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+	end
 end
