bc3001c5344e35981c39493fa115f111e1ee72ec
sanoyo <sa00380304yo@gmail.com>
2018-11-11 20:30:16 +0900

    Remove Photos with AJAX

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,4 +11,15 @@ class PhotosController < ApplicationController
       redirect_back(fallback_location: request.referer, notice: 'Saved')
     end
   end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+
+    room = @photo.room
+
+    @photo.destroy
+    @photos = Photo.where(room_id: room.id)
+
+    response_to :js
+  end
 end
